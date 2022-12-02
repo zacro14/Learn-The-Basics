c174aeba73bb5525af387a9d73f528a6182da816
junel <sacrojunel@gmail.com>
2022-12-02 21:21:04 +0800

    feat: add naviagtion links

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,25 +1,90 @@
 'use client'
-import { Box } from '@chakra-ui/react'
+import {
+    Box,
+    Divider,
+    List,
+    ListIcon,
+    ListItem,
+    VStack,
+} from '@chakra-ui/react'
+import {
+    Squares2X2Icon,
+    ListBulletIcon,
+    Cog6ToothIcon,
+} from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import React from 'react'
 
-export default function DashboardLayout() {
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const navigation = [
+        {
+            name: 'Dashboard',
+            link: '/dashboard',
+            icon: Squares2X2Icon,
+        },
+        {
+            name: 'Category',
+            link: '/dashboard/category',
+            icon: ListBulletIcon,
+        },
+    ]
     return (
-        <Box bgColor={'gray.100'} position="relative">
+        <Box bgColor={'gray.100'} position="relative" display={'flex'}>
             <Box
-                position={'fixed'}
+                pos={'sticky'}
                 as="nav"
                 w="80"
-                h="container.xl"
                 bgColor={'white'}
                 boxShadow="md"
             >
-                <VStack alignItems={'start'} p="5">
-                    <UnorderedList>
-                        <ListItem>Dashboard</ListItem>
-                        <ListItem>Consectetur adipiscing elit</ListItem>
-                        <ListItem>Integer molestie lorem at massa</ListItem>
-                        <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-                    </UnorderedList>
+                <VStack alignItems={'start'} p="5" mt={'20'}>
+                    <List width="full">
+                        {navigation.map(({ name, link, icon }, index) => (
+                            <ListItem
+                                key={index}
+                                rounded={'lg'}
+                                p={'5'}
+                                width={'full'}
+                                cursor="pointer"
+                                _hover={{ bgColor: 'gray.100' }}
+                            >
+                                <ListIcon
+                                    as={icon}
+                                    color="green.500"
+                                    boxSize={'5'}
+                                    mr="4"
+                                />
+                                <Link href={link}>{name}</Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider p={'3'} />
+
+                    <List width={'full'}>
+                        <ListItem
+                            rounded={'lg'}
+                            p={'5'}
+                            width={'full'}
+                            cursor="pointer"
+                            _hover={{ bgColor: 'gray.100' }}
+                        >
+                            <ListIcon
+                                as={Cog6ToothIcon}
+                                color="green.500"
+                                boxSize={'5'}
+                                mr="4"
+                            />{' '}
+                            Settings
+                        </ListItem>
+                    </List>
                 </VStack>
+            </Box>
+            <Box as="section" flex={'1'}>
+                {children}
             </Box>
         </Box>
     )
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
+'use client'
+import { Box, Text } from '@chakra-ui/react'
+
 export default function Dashboard() {
-    return <div>dashboard</div>
+    return (
+        <Box sx={{ minHeight: '100vh' }} bgColor="gray.100">
+            {Array.apply(0, Array(50)).map(function (x, i) {
+                return <Text key={i}>Helo helo</Text>
+            })}
+        </Box>
+    )
 }
