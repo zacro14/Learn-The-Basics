b1400c65825a81f6821cf264bb86810009ac9932
junel <sacrojunel@gmail.com>
2022-12-14 10:07:28 +0800

    feat: add validation on sign up

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -9,9 +9,13 @@ import {
     FormErrorMessage,
     FormLabel,
     Heading,
+    Icon,
     Input,
     Link,
+    ListItem,
     Text,
+    Tooltip,
+    UnorderedList,
     VStack,
 } from '@chakra-ui/react';
 import NextLink from 'next/link';
@@ -19,20 +23,18 @@ import AuthContainer from 'component/Container/Auth/Auth';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 
 type Signup = yup.TypeOf<typeof schema>;
-// firstName: string;
-// lastName: string;
-// userName: string;
-// email: string;
-// password: string;
-// isAgreeTerms: boolean;
 
 const schema = yup.object({
     firstName: yup.string().required('Please enter your first name.'),
     lastName: yup.string().required('Please enter your lastname.'),
     userName: yup.string().required('Please enter your username.'),
-    email: yup.string().email('Please enter an email.'),
+    email: yup
+        .string()
+        .required('Please enter your email.')
+        .email('Please enter an email.'),
     password: yup
         .string()
         .required('password is required.')
@@ -49,12 +51,12 @@ export default function Signup() {
         setValue,
         handleSubmit,
         watch,
-        formState: { errors },
+        formState: { errors, isDirty, isValid },
     } = useForm<Signup>({ resolver: yupResolver(schema) });
 
-    const onSubmitHandler: SubmitHandler<Signup> = handleSubmit(
-        (data: Signup) => console.log(data)
-    );
+    const onSubmitHandler: SubmitHandler<Signup> = (data) => {
+        console.log(data);
+    };
 
     console.log(errors.userName);
 
@@ -81,36 +83,112 @@ export default function Signup() {
                                 </FormErrorMessage>
                             )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={errors.email ? true : false}>
                             <FormLabel>Email</FormLabel>
                             <Input
                                 placeholder="jhondoe@email.com"
                                 type={'email'}
                                 {...register('email')}
                             />
+                            {errors.email && (
+                                <FormErrorMessage>
+                                    {errors.email?.message}
+                                </FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl
+                            isInvalid={errors.firstName ? true : false}
+                        >
                             <FormLabel>First Name</FormLabel>
                             <Input
                                 placeholder="eg. Jhon"
                                 {...register('firstName')}
                             />
+                            {errors.firstName && (
+                                <FormErrorMessage>
+                                    {errors.firstName?.message}
+                                </FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
+                        <FormControl isInvalid={errors.lastName ? true : false}>
                             <FormLabel>Last Name</FormLabel>
                             <Input
                                 placeholder="eg. Doe"
                                 {...register('lastName')}
                             />
+                            {errors.lastName && (
+                                <FormErrorMessage>
+                                    {errors.lastName?.message}
+                                </FormErrorMessage>
+                            )}
                         </FormControl>
-                        <FormControl>
-                            <FormLabel>Password</FormLabel>
+                        <FormControl isInvalid={errors.password ? true : false}>
+                            <FormLabel
+                                display={'flex'}
+                                alignItems={'center'}
+                                gap={'1'}
+                            >
+                                <Text>Password</Text>
+                                <Tooltip
+                                    placement={'right'}
+                                    hasArrow
+                                    label={
+                                        <Box
+                                            p={'2'}
+                                            fontSize={'xs'}
+                                            rounded={'2xl'}
+                                        >
+                                            <Text
+                                                fontSize={'xs'}
+                                                color={'black'}
+                                                as={'span'}
+                                                fontWeight={'bold'}
+                                            >
+                                                Password must have
+                                            </Text>
+                                            <UnorderedList
+                                                pl={'5'}
+                                                color={'blackAlpha.800'}
+                                            >
+                                                <ListItem>
+                                                    at least eight characters
+                                                    long;
+                                                </ListItem>
+                                                <ListItem>
+                                                    a lowercase letter;
+                                                </ListItem>
+                                                <ListItem>
+                                                    an uppercase letter;
+                                                </ListItem>
+                                                <ListItem>
+                                                    a special character like
+                                                    ".!@#$%^&*()+-=,".
+                                                </ListItem>
+                                            </UnorderedList>
+                                        </Box>
+                                    }
+                                    bg={'white'}
+                                    boxShadow={'2xl'}
+                                >
+                                    <Icon
+                                        as={InformationCircleIcon}
+                                        color={'gray.500'}
+                                    />
+                                </Tooltip>
+                            </FormLabel>
+
                             <Input
                                 type={'password'}
                                 autoComplete="current-password"
+                                placeholder="Create a password"
                                 pr="4.5rem"
                                 {...register('password')}
                             />
+                            {errors.password && (
+                                <FormErrorMessage>
+                                    {errors.password?.message}
+                                </FormErrorMessage>
+                            )}
                         </FormControl>
                         <Checkbox
                             colorScheme={'green'}
