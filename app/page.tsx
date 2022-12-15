6b5e4a3c628e873ebc88d29649a62674e7c35bef
junel <sacrojunel@gmail.com>
2022-12-15 19:16:07 +0800

    chore: add zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
-import { Button, Flex } from '@chakra-ui/react';
 import { AppHeader } from 'component/Header/AppHeader';
-import Link from 'next/link';
 
 export default function Home() {
     return (
