4751eea67be52f60e9f7813ad2d29f6ca2995f8c
junel <sacrojunel@gmail.com>
2022-12-26 22:11:31 +0800

    chore: add todo for private api config

--- lib/axios/Api.ts
+++ lib/axios/Api.ts
@@ -3,6 +3,7 @@ export const ApiClientPublic = axios.create({
     baseURL: process.env.API_BASE_URL,
 });
 
+//TODO create an authorization bearer for private api endpoints
 export const ApiClientPrivate = axios.create({
     withCredentials: true,
     baseURL: process.env.API_BASE_URL,
