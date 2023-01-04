2591badad7a271e70c381a8aef6b196180ff175e
junel <sacrojunel@gmail.com>
2023-01-04 20:31:48 +0800

    feat: add tip tap for rich text editor

--- app/dashboard/lessons/page.tsx
+++ app/dashboard/lessons/page.tsx
@@ -1,6 +1,15 @@
 'use client';
 import { Box } from '@chakra-ui/react';
-
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 export default function Lessons() {
-    return <Box p={'5'}>lessons</Box>;
+    const editor = useEditor({
+        extensions: [StarterKit],
+        content: '<p>Hello World! 🌎️</p>',
+    });
+    return (
+        <Box p={'5'}>
+            <EditorContent editor={editor}></EditorContent>
+        </Box>
+    );
 }
