b42165f437f3d1877b5e885b9343985e6a9fe38c
junel <sacrojunel@gmail.com>
2023-01-08 21:42:00 +0800

    feat(theme): extend chakra ui theme

---
+++ theme/components/index.ts
@@ -0,0 +1,7 @@
+import { ThemeComponents } from '@chakra-ui/react';
+import { Input } from './input';
+const components: ThemeComponents = {
+    Input,
+};
+
+export default components;
--- theme/theme.ts
+++ theme/theme.ts
@@ -1,57 +1,25 @@
-import { extendTheme } from '@chakra-ui/react';
+import { ChakraTheme, extendTheme } from '@chakra-ui/react';
+import components from './components';
+import { Styles } from '@chakra-ui/theme-tools';
 
+const styles: Styles = {
+    global: (props) => ({
+        shadows: {
+            outline: '0 0 0 1px green.500',
+        },
+    }),
+};
+
+const themeOvirrides: Partial<ChakraTheme> = {
+    components,
+    styles,
+};
 const theme = extendTheme({
     fonts: {
         heading: `'Opens Sans', sans-serif`,
         body: `'Poppins', 'sans-serif'`,
     },
-    styles: {
-        shadows: {
-            outline: '0 0 0 1px #E53E3E',
-        },
-    },
-    components: {
-        Textarea: {
-            variants: {
-                outline: {
-                    field: {
-                        _focus: {
-                            borderColor: 'green.500',
-                            boxShadow: '0 0 0 1px green.500',
-                        },
-                    },
-                },
-                filled: {
-                    field: {
-                        _focus: {
-                            borderColor: 'green.500',
-                            boxShadow: '0 0 0 1px green.500',
-                        },
-                    },
-                },
-            },
-        },
-        Input: {
-            variants: {
-                outline: {
-                    field: {
-                        _focus: {
-                            borderColor: 'green.500',
-                            boxShadow: '0 0 0 1px green.500',
-                        },
-                    },
-                },
-                filled: {
-                    field: {
-                        _focus: {
-                            borderColor: 'green.500',
-                            boxShadow: '0 0 0 1px green.500',
-                        },
-                    },
-                },
-            },
-        },
-    },
+    ...themeOvirrides,
 });
 
 export default theme;
