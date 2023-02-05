763e1c84a38cff20201d63bc305a06babc0bb3e9
junel <sacrojunel@gmail.com>
2023-02-05 21:04:21 +0800

    feat: add functionality for the menu buttons

--- component/editor/Editor.tsx
+++ component/editor/Editor.tsx
@@ -1,6 +1,10 @@
 import Placeholder from '@tiptap/extension-placeholder';
 import { Box, IconButton, Tooltip } from '@chakra-ui/react';
-import { EditorContent, useEditor } from '@tiptap/react';
+import {
+    Editor as TiptapEditor,
+    EditorContent,
+    useEditor,
+} from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import {
     FaBold,
@@ -13,25 +17,50 @@ import {
     FaCode,
     FaUnderline,
 } from 'react-icons/fa';
+import { useState } from 'react';
 
-function EditoMenu() {
+type EditorProps = {
+    editor: TiptapEditor | null;
+};
+
+function EditMenu({ editor }: EditorProps) {
+    // here we return null if no intance of the editor
+    if (!editor) return null;
+
     const Icons = [
-        { icon: <FaBold />, label: 'bold' },
-        { icon: <FaItalic />, label: 'italic' },
-        { icon: <FaListOl />, label: 'ordered list' },
-        { icon: <FaListUl />, label: 'unordered list' },
-        { icon: <FaHeading />, label: 'heading' },
-        { icon: <FaLink />, label: 'link' },
-        { icon: <FaCode />, label: 'code' },
-        { icon: <FaQuoteLeft />, label: 'qoute' },
-        { icon: <FaUnderline />, label: 'underline' },
+        { icon: <FaBold />, label: 'bold', name: 'bold' },
+        { icon: <FaItalic />, label: 'italic', name: 'italic' },
+        { icon: <FaListOl />, label: 'ordered list', name: 'ol' },
+        { icon: <FaListUl />, label: 'unordered list', name: 'ul' },
+        { icon: <FaHeading />, label: 'heading', name: 'heading' },
+        { icon: <FaLink />, label: 'link', name: 'link' },
+        { icon: <FaCode />, label: 'code', name: 'code' },
+        { icon: <FaQuoteLeft />, label: 'qoute', name: 'qoute' },
+        { icon: <FaUnderline />, label: 'underline', name: 'underline' },
     ];
+
+    const handleCommand = (name: string) => {
+        const commands: { [key: string]: () => void } = {
+            bold: () => editor.chain().toggleBold().run(),
+            italic: () => editor.chain().focus().toggleItalic().run(),
+            ol: () => editor.chain().toggleOrderedList().run(),
+            ul: () => editor.chain().toggleBulletList().run(),
+        };
+
+        if (commands[name]) {
+            commands[name]();
+        }
+    };
+
     return (
         <Box bgColor={'gray.50'} p={'3'}>
-            {Icons.map(({ icon, label }, i) => (
+            {Icons.map(({ icon, label, name }, i) => (
                 <Tooltip key={i} label={label} rounded={'md'}>
                     <span>
                         <IconButton
+                            mr={'1'}
+                            isActive={editor.isActive(name) ? true : false}
+                            onClick={() => handleCommand(name)}
                             variant={'ghost'}
                             aria-label={label}
                             icon={icon}
@@ -62,7 +91,7 @@ export const Editor = () => {
 
     return (
         <Box border={'2px'} rounded={'md'} minH={'40'}>
-            <EditoMenu />
+            <EditMenu editor={editor} />
             <EditorContent editor={editor} />
         </Box>
     );
